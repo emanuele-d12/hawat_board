c3ebaebb653e7c9708b11f0a9e050368c51925f9
Emanuele <emanuele.diarena@gmail.com>
2026-05-30 16:31:56 +0200

    added PORT to backend env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const DATA_DIR = path.resolve(
 app.use(cors())
 app.use(express.json())
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
